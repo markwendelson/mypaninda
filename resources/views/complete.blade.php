aad0aab74ebed945891f74c69a1644edbba5fa0e
Ced <34019610+technoblas@users.noreply.github.com>
2023-05-04 09:53:12 +0800

    Update complete order message

--- resources/views/complete.blade.php
+++ resources/views/complete.blade.php
@@ -6,7 +6,7 @@
         <div class="card py-3 mt-sm-3">
           <div class="card-body text-center">
             <h3 class="h4 pb-3">Thank you for your order!</h3>
-            <p class="mb-2">Your order has been placed and will be processed as soon as possible.</p>
+            <p class="mb-2">Your order # {{ $order->id }} has been placed and will be processed as soon as possible.</p>
             <p class="mb-2">Make sure you make note of your payment code, which is <strong>{{ $order->payment_code }}</strong>.</p>
             {{-- <p>You will be receiving an email shortly with confirmation of your order. <u>You can now:</u></p> --}}
             <a class="btn btn-secondary mt-3 mr-3" href="{{ route('home') }}">Go back shopping</a>
