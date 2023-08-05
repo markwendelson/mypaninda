6ce4d632045e9e5e1daa2e19c6d08cb47d4c15e0
technoblas <technoblas@gmail.com>
2023-08-05 11:50:41 +0800

    Added product display condition

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -12,16 +12,18 @@ class ShopController extends Controller
     {
         $categoryId = $request->query('category');
         $category = Category::findOrFail($categoryId);
-        $products = Product::where('category_id', $categoryId)->paginate(16)
-            ->appends('category', $categoryId);
+        $products = Product::where('category_id', $categoryId)
+                    ->where('stocks', '>', 0)->where('status', 1)
+                    ->paginate(16)->appends('category', $categoryId);
 
         return view('shop', compact('category', 'products'));
     }
 
     public function item($id)
     {
-        $product = Product::findOrFail($id);
-        $recommendations = Product::inRandomOrder()->limit(12)->get();
+        $product = Product::where('id', $id)->where('stocks', '>', 0)->where('status', 1)->firstOrFail();
+        $recommendations = Product::where('stocks', '>', 0)->where('status', 1)
+            ->inRandomOrder()->limit(12)->get();
 
         return view('item', compact('product', 'recommendations'));
     }
@@ -29,8 +31,12 @@ class ShopController extends Controller
     public function search(Request $request)
     {
         $search = $request->query('q');
-        $products = Product::where('title', 'LIKE', '%'.$search.'%')
-            ->orWhere('brand', 'LIKE', '%'.$search.'%')
+        $products = Product::where(function ($query) use ($search){
+                $query->where('title', 'LIKE', '%'.$search.'%')
+                ->orWhere('brand', 'LIKE', '%'.$search.'%');
+            })
+            ->where('stocks', '>', 0)
+            ->where('status', 1)
             ->oldest()
             ->paginate(16)
             ->appends('q', $search);
