d10f669aea432b52ff975534000536322b7dcd47
technoblas <technoblas@gmail.com>
2023-08-11 16:28:59 +0800

    Added webhook for payments

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,13 +10,37 @@ use App\Models\Product;
 
 class WebhookController extends Controller
 {
-    public function paymentStatus(Request $request)
+    public function payment(Request $request)
     {
-        // update db status
-        // restore product stocks if status is expired or failed
+        // update db status, validate request
+        // sample api payload {order_id, payment_code, status}
+        $id = $request->order_id;
+        $paymentCode = $request->payment_code;
+        $status = $request->status;
+
+        $order = Order::where('id', $id)
+            ->where('payment_code', $paymentCode)
+            ->where('status', 'pending')
+            ->firstOrFail();
+        $order->status = strtolower($status);
+        $order->save();
+
+        // if status is expired, cancelled then restore stocks
+        if (in_array($status, ['expired', 'cancelled'])) {
+            $items = OrderItem::where('order_id', $id)->select('id', 'order_id', 'product_id', 'quantity')->get();
+            foreach ($items as $item) {
+                Product::find($item->product_id)->increment('stocks', $item->quantity);
+            }
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'order_id' => $id,
+            'payment_code' => $paymentCode,
+        ]);
     }
 
-    public function deliveryStatus(Request $request)
+    public function delivery(Request $request)
     {
         // update db status
     }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// for demo purposes only use POST
+Route::get('/webhook/payments', [App\Http\Controllers\WebhookController::class, 'payment']);
