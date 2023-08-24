4e68ed7affc22972e7789984ba408b7c340de9a0
Mark Wendelson R. Olog <markwendelson@gmail.com>
2023-08-25 00:15:01 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes([ 'verify' => true]);
 
